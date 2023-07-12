dfde51fbcb5e43c6bb31a7a5d50905a90bd85a8b
Stella Johansson <stellajoh9@gmail.com>
2023-07-12 21:41:49 +0200

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Stella is the champion!";
+newHeader.className = "victory";
